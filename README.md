67d5e0c7a34728ebe5c6d23e0c7b1e92ab1f3b85
𝗞𝗿𝗶𝘀𝘇𝘁𝗶𝗮𝗻 𝗣𝗹𝗲𝘁𝘀𝗲𝗿 <krisztianpletser62@gmail.com>
2026-01-20 10:32:16 +0100

    debloater-test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Windows-11-Debloater-V2
-Windows-11-Debloater-V2
+# Win11 Debloater GUI
+Run with .NET 8 + WinUI 3
---
+++ Win11DebloaterGUI/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace Win11DebloaterGUI;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
